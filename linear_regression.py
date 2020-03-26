5a2cc4f5530c28ccc08ac8a04642a454e862728e
hontarenkoYana <yana.hontarenko@gmail.com>
2020-03-26 17:39:26 +0200

    Add mae loss

--- linear_regression.py
+++ linear_regression.py
@@ -25,20 +25,22 @@ def data():
     return X, y
 
 
-def draw(X, y, learned_coef, sklearn_coef):
+def draw(X, y, learned_coef_mse, learned_coef_mae, sklearn_coef):
     """
     Function to draw results.
     :param X: feature vector
     :param y: target value
-    :param learned_coef: learned by custom function coefficient
-    :param sklearn_coef: learned by sklearn function coefficient
+    :param learned_coef_mse: learned by custom function with mse loss coefficient
+    :param learned_coef_mae: learned by sklearn function with mae coefficient
     """
-    print(f"Learned by custom function coefficient: {learned_coef[1, 0]}, bias: {learned_coef[0, 0]}")
-    print(f"Learned by sklearn function coefficient: {sklearn_coef[1, 0]}, bias: {sklearn_coef[0, 0]}")
+    print(f"Learned by custom function coefficient: {learned_coef_mse[1, 0]}, bias: {learned_coef_mse[0, 0]}")
+    print(f"Learned by sklearn function coefficient: {learned_coef_mae[1, 0]}, bias: {learned_coef_mae[0, 0]}")
     plt.scatter(X, y, color='black')
     plt.legend(['data'])
-    plt.plot(X, X * learned_coef[1, 0] + learned_coef[0, 0], color="blue")
-    plt.legend(['My linear regression'])
+    plt.plot(X, X * learned_coef_mse[1, 0] + learned_coef_mse[0, 0], color="blue")
+    plt.legend(['My linear regression with mse'])
+    plt.plot(X, X * learned_coef_mae[1, 0] + learned_coef_mae[0, 0], color="green")
+    plt.legend(['My linear regression with mae'])
     plt.plot(X, X * sklearn_coef[1, 0] + sklearn_coef[0, 0], color="red")
     plt.legend(['Sklearn linear regression'])
     plt.show()
@@ -73,7 +75,28 @@ def mse_derivative(predicted, true, X):
     return (2/(predicted.shape[0])) * np.sum((predicted - true) * X)
 
 
-def gradient_descent(theta0, theta1, precision, l_r, X, y):
+def mae(predicted, true):
+    """
+    :param predicted: predicted values
+    :param true: target values
+    :return: mean absolute error between true and predicted values
+    """
+    return 1/(2 * predicted.shape[0]) * np.sum(np.abs(predicted - true))
+
+
+def mae_derivative(predicted, true, X):
+    """
+    :param predicted: predicted values
+    :param true: target values
+    :param X: feature vector
+    :return: derivative of mean squared error between true and predicted values
+    """
+    greater = predicted > true
+    greater = np.where(greater, 1, -1)
+    return np.sum(greater)
+
+
+def gradient_descent(theta0, theta1, precision, l_r, X, y, loss="mse"):
     """
     Optimization of loss mse function for linear regression with gradient descend algorithm.
     :param theta0: bias variable (zero coefficient of linear regression)
@@ -82,28 +105,36 @@ def gradient_descent(theta0, theta1, precision, l_r, X, y):
     :param l_r: learning rate - step size at each iteration
     :param X: feature vector
     :param y: target values vector
+    :param loss: loss function
     :return: new coefficients of linear regression
     """
+    if loss == "mse":
+        derivative = mse_derivative
+    else:
+        derivative = mae_derivative
     prediction = linear_regression(X, theta0, theta1)
-    theta0_loss_df = - mse_derivative(prediction, y, 1)
-    theta1_loss_df = - mse_derivative(prediction, y, X)
+    theta0_loss_df = -derivative(prediction, y, 1)
+    theta1_loss_df = -derivative(prediction, y, X)
     theta0_new = theta0 + (l_r * theta0_loss_df)
     theta1_new = theta1 + (l_r * theta1_loss_df)
-    while norm(theta1_new - theta1) > precision:
+    k = 1
+    while norm(theta1_new - theta1) > precision and k < 100:
         theta0 = theta0_new
         theta1 = theta1_new
         prediction = linear_regression(X, theta0, theta1)
-        theta0_loss_df = - mse_derivative(prediction, y, 1)
-        theta1_loss_df = - mse_derivative(prediction, y, X)
+        theta0_loss_df = -derivative(prediction, y, 1)
+        theta1_loss_df = -derivative(prediction, y, X)
         theta0_new = theta0 + (l_r * theta0_loss_df)
         theta1_new = theta1 + (l_r * theta1_loss_df)
+        k += 1
 
     return np.array([theta0_new, theta1_new]).reshape(-1, 1)
 
 
 X, y = data()
-learned_coef = gradient_descent(np.array([1.5]).reshape(-1, 1), np.array([1.5]).reshape(-1, 1), 0.001, 0.05, X, y)
+learned_coef_mse = gradient_descent(np.array([1.5]).reshape(-1, 1), np.array([1.5]).reshape(-1, 1), 0.001, 0.05, X, y, "mse")
+learned_coef_mae = gradient_descent(np.array([1.5]).reshape(-1, 1), np.array([1.5]).reshape(-1, 1), 0.001, 0.05, X, y, "mae")
 lr = LinearRegression()
 lr.fit(X, y)
 sklearn_coef = np.array([lr.intercept_, lr.coef_])
-draw(X, y, learned_coef, sklearn_coef)
+draw(X, y, learned_coef_mse, learned_coef_mae, sklearn_coef)
